b8fb9135585c6540dbba5e16ccd804c4ca2c088c
tianyi-m <inipop.tianyi@gmail.com>
2021-12-04 15:22:57 -0500

    filter backend

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../models/project");
 
-//Get all projects
+// Get all projects
 router.get("/", function (req, res) {
     Project.find()
+      .then((projects) => {
+        res.send(projects);
+      });
+  });
+
+// Get project based on search criteria
+router.post("/", function (req, res) {
+    const data = req.body; // is a json file specifies criterias like {"lgbtq":"Yes", "projectName":"projectName"}
+    const query = {}
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            if (key == "project name"){
+                query[key] = '/'+data[key]+'/';
+            } else {
+                query[key] = data[key];
+            }
+        }
+    }
+    Project.find(query)
       .then((projects) => {
         res.send(projects);
       });
