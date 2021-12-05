b689575baa4fb11050b061a76893b45c30e3c440
rajakiru <raja.kiruthika@yahoo.com>
2021-12-05 18:16:29 +0900

    my commit

--- src/App.js
+++ src/App.js
@@ -153,9 +153,9 @@ class App extends React.Component {
         <section id="scroll">
           <div class="projects_container">
           {/* {results} */}
-          <div class="project_item">
+            <div class="project_item">
               <h3> Project Name </h3>
-              <h5> Project Owner <p> (she/her) </p> </h5>
+              <h5> Project Owner <p id="text1"> (she/her) </p> </h5>
               <p> Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
               <details>
               <summary>
@@ -163,10 +163,8 @@ class App extends React.Component {
                <span id="close">close</span>
               </summary>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-
-                
               </details>
-          </div>
+            </div>
           </div>
         </section> 
 
